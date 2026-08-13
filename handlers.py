3494451f8c7675be6607b131ab380869b177e7b0
Stan Thomas <161471399+Tstan08210@users.noreply.github.com>
2026-08-13 07:43:54 +0530

    Update handlers.py
    Changed to new account

--- handlers.py
+++ handlers.py
@@ -1,7 +1,7 @@
 def admin_only(func):
     async def wrapper(update, context, *args, **kwargs):
         admins = await context.bot.get_chat_administrators(update.effective_chat.id)
-        is_admin = any(admin.user.id == update.effective_user.id for admin in admins) or update.effective_user.id == 5364976682
+        is_admin = any(admin.user.id == update.effective_user.id for admin in admins) or update.effective_user.id == 8875657209
         if is_admin:
             await func(update, context, *args, **kwargs)
         else:
